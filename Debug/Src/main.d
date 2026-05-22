b22f9d082444a23a41871f0b7347e6be429cf7e8
Meena DS <meena.ds@bitsilica.com>
2026-05-22 06:06:09 +0530

    Re-add all STM32 projects

--- Debug/Src/main.d
+++
@@ -1,3 +0,0 @@
-Src/main.o: ../Src/main.c ../Inc/uart.h ../Inc/stm32f407xx.h
-../Inc/uart.h:
-../Inc/stm32f407xx.h:
--- Debug/Src/uart.d
+++
@@ -1,3 +0,0 @@
-Src/uart.o: ../Src/uart.c ../Inc/uart.h ../Inc/stm32f407xx.h
-../Inc/uart.h:
-../Inc/stm32f407xx.h:
